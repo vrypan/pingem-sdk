6f3770d196e5054f55787d388951500ca2628314
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2025-04-28 12:50:13 +0300

    New fix...

--- src/fetch-node.ts
+++ src/fetch-node.ts
@@ -1,4 +1,4 @@
-export async function fetchNode(url: string, options: any) {
-  const { fetch } = await import('undici');
-  return fetch(url, options);
+export async function fetchNode(url: string, options: RequestInit) {
+  const undici = (await import("undici")) as typeof import("undici");
+  return undici.fetch(url, options);
 }
